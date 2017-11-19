ac29642c821b68116f73fe182aec019a5fc59bd9
ellaf <league@WTS-iMac-11.attlocal.net>
2017-11-19 11:28:42 -0800

    Finished longest word, contains SOS, and almost finished sort scores! :)

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -5,48 +5,58 @@ import java.util.List;
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
 		for (int i = 0; i < eggs.size(); i++) {
-			
-		if(eggs.get(i).equals("cracked")) {
-			return i;
+
+			if (eggs.get(i).equals("cracked")) {
+				return i;
+			}
 		}
-		}
-		
 		return -1;
-		 //<- this needs changing
+		
 	}
-	
+
 	public static int countPearls(List<Boolean> oysters) {
 		int totalPearls = 0;
 		for (int i = 0; i < oysters.size(); i++) {
-		if(oysters.get(i) == true) {
-		totalPearls += 1;
-		}
+			if (oysters.get(i) == true) {
+				totalPearls += 1;
+			}
 		}
 		return totalPearls;
-		
+
 	}
-	
+
 	public static double findTallest(List<Double> peeps) {
 		double biggest = 0;
 		for (int i = 0; i < peeps.size(); i++) {
-		if(peeps.get(i) >= biggest) {
-		biggest = peeps.get(i);
-		}
+			if (peeps.get(i) >= biggest) {
+				biggest = peeps.get(i);
+			}
 		}
 		return biggest;
 	}
-	
-	
+
 	public static String findLongestWord(List<String> words) {
 		int longest = 0;
 		String longestString = "";
 		for (int i = 0; i < words.size(); i++) {
-		if(words.get(i).length() >= longest) {
-		longest = words.get(i).length();
-		longestString = words.get(i);
-		}
+			if (words.get(i).length() > longest) {
+				longest = words.get(i).length();
+				longestString = words.get(i);	
+				System.out.println(longestString);
+			}
 		}
 		return longestString;
+	
 	}
-	//Add other methods here
+	
+	
+	public static boolean containsSOS(List<String> f) {
+	System.out.println(f);
+		if(f.contains(" ... --- ... ")){
+				return true;
+		}
+		return false;
+		
+	}
+	// Add other methods here
 }
